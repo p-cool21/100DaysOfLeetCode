657c49fd0ac379f7d3ff551e9d4f32d50622092f
p-cool21 <priyankatagad21@gmail.com>
2025-10-20 20:08:32 +0530

    Day08: Add Array Partition Solution

---
+++ array_partition.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()  
+        answer = 0
+        for i in range(0, len(nums), 2):
+            answer += nums[i]
+        return answer
