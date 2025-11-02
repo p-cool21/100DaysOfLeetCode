c2306be08c307cb671c2ad6fb2d15cf76568a713
p-cool21 <priyankatagad21@gmail.com>
2025-11-02 12:41:03 +0530

    Day17: Solution okf valid palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        cleaned = ''.join(ch.lower() for ch in s if ch.isalnum())
+        return cleaned == cleaned[::-1]
