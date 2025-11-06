89178bdc88ae1381050971fdf76bdeff4ae6f6b2
p-cool21 <priyankatagad21@gmail.com>
2025-11-06 17:19:51 +0530

    Day20: solution of longest repeating characters replacemnt

---
+++ longest_repeating_characters_replacement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        count = {}
+        left = 0
+        max_count = 0
+        result = 0
+
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            max_count = max(max_count, count[s[right]])
+            while (right - left + 1) - max_count > k:
+                count[s[left]] -= 1
+                left += 1
+
+            result = max(result, right - left + 1)
+
+        return result
